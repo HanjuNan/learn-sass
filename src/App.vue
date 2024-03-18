045684cd52b6312f27cca937a0566051ca6a6a95
hanjunan <1401224905@qq.com>
2024-03-19 06:29:46 +0800

    学习sass的使用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="btn">
+      盒子
+      <div>sass使用</div>
+      <h1>我是大标题<span>财务管理</span></h1>
+    </div>
   </div>
 </template>
 
@@ -17,6 +22,27 @@ export default {
 </script>
 
 <style lang="scss">
+//1.标签的嵌套
+//2.声明变量 sass -> $
+
+$color: black;
+$color2: skyblue;
+
+.btn {
+  color: red;
+  h1 {
+    color: blue;
+    span {
+      color: yellow;
+    }
+    &:hover {
+    color: $color2;
+  }
+  }
+  
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
